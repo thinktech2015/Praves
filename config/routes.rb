6d1165f9dbcb83a8afe3a7aaf1a677f92eacd5ed
Varun <varunmundra.iitkgp@gmail.com>
2014-08-21 12:32:25 +0530

    route changed

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ FirstApp::Application.routes.draw do
   get "static_pages/index"
   get "static_pages/help"
   get "static_pages/services"
+  get "static_pages/career"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
