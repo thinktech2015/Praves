486deadecfabcbc92df462eed4fff25eaf88e484
Varun <varunmundra.iitkgp@gmail.com>
2014-09-03 12:19:00 +0530

    change mobile type

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140902073911) do
+ActiveRecord::Schema.define(version: 20140903064627) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.integer  "mobile"
+    t.string   "mobile"
     t.string   "qualification"
     t.string   "message"
     t.datetime "created_at"
