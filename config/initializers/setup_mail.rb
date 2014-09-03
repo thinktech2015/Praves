f28738e5c9653fa36073a69a35834b2f8a872c79
Varun <varunmundra.iitkgp@gmail.com>
2014-09-03 12:07:55 +0530

    changes for mailer on heroku

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,13 +1,4 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "mail.google.com",
-  :user_name            => "priyanko.dey33@gmail.com",
-  :password             => "priyanko123456",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
-
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
-
-
+if Rails.env != 'test'
+  email_settings = YAML::load(File.open("#{Rails.root.to_s}/config/email.yml"))
+  ActionMailer::Base.smtp_settings = email_settings[Rails.env] unless email_settings[Rails.env].nil?
+end
